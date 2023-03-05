7a1c0846f97d01a2ca2557a09f513bd7d6753a25
Amol shelke <shelkeamol897@gmail.com>
2023-03-05 23:50:12 +0530

    Added Navbar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,31 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Paper, IconButton } from "@mui/material";
-import { SearchIcon } from "@mui/icons-material";
+import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        p1: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}>
+      <input
+        className="search-bar"
+        placeholder="Search.."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
 };
 
 export default SearchBar;
