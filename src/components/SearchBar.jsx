57c02617d7d20c37446f9e63013ef764f3799c5a
Amol shelke <shelkeamol897@gmail.com>
2023-03-05 23:35:27 +0530

    Creating SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Paper, IconButton } from "@mui/material";
+import { SearchIcon } from "@mui/icons-material";
 
 const SearchBar = () => {
   return <div>SearchBar</div>;
