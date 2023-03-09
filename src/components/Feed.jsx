0f64f360acaa2c5036d744642d6bef7436d5ac11
Amol shelke <shelkeamol897@gmail.com>
2023-03-09 22:51:24 +0530

    Updated Feed component and Fetching Videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -22,6 +22,19 @@ const Feed = () => {
         >
           Copyright 2023 Amol Shelke
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          New <span style={{ color: `#F31503` }}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
