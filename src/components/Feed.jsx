4d8e9320204b408de55adaea738f47ff03dd0b62
Amol shelke <shelkeamol897@gmail.com>
2023-03-09 23:48:33 +0530

    Fetched data from rapid API and share it via props to other component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Sidebar, Videos } from './';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then(data =>
+      setVideos(data.items),
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
       <Box
@@ -14,7 +25,11 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
         <Typography
           className="copyright"
           variant="body2"
@@ -31,10 +46,10 @@ const Feed = () => {
           mb={2}
           sx={{ color: 'white' }}
         >
-          New <span style={{ color: `#F31503` }}>Videos</span>
+          {selectedCategory} <span style={{ color: `#F31503` }}>Videos</span>
         </Typography>
 
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
