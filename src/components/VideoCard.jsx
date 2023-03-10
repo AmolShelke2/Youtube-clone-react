e90ad7ff936ed49d90826982481aff1182ad2690
Amol shelke <shelkeamol897@gmail.com>
2023-03-11 01:21:07 +0530

    Removed unused imports

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,7 +4,6 @@ import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
 import {
-  demoThumbnailUrl,
   demoVideoUrl,
   demoVideoTitle,
   demoChannelUrl,
