f836f2d0226215488f323cf72ad20f1fe5a122fd
Amol shelke <shelkeamol897@gmail.com>
2023-03-10 23:54:53 +0530

    Added VideoCard and ChannelCard componetn

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,5 +1,7 @@
 import { Stack, Box } from '@mui/material';
 
+import { VideoCard, ChannelCard } from './';
+
 const Videos = ({ videos }) => {
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
