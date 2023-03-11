25bd1a1bc65e09375823922b6053a3939b2a833e
Amol shelke <shelkeamol897@gmail.com>
2023-03-11 23:37:06 +0530

    Creating Channel detailscomponent

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
 
 const ChannelDetail = () => {
   return <div>ChannelDetail</div>;
