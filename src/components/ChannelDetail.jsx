70d9ced28b185023ef08b9c5a109df9f900816f8
Amol shelke <shelkeamol897@gmail.com>
2023-03-11 23:50:09 +0530

    Added ChannelDetail component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -3,9 +3,17 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setchannelDetail] = useState(null);
   const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`).then(data =>
+      setchannelDetail(data?.items[0]),
+    );
+  }, [id]);
 
   return <div>{id}</div>;
 };
