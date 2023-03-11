89d7a7b7995d252c4f68e35f50b2c0c65a12d3c6
Amol shelke <shelkeamol897@gmail.com>
2023-03-12 00:44:58 +0530

    updated ChannelDetail Component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { Videos, ChannelCard } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos, ChannelCard } from './';
 
 const ChannelDetail = () => {
   const [channelDetail, setchannelDetail] = useState(null);
