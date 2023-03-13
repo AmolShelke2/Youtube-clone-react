410ab2f0e97ec1436ae1d17d8864b5ae53061d01
Amol shelke <shelkeamol897@gmail.com>
2023-03-13 23:23:22 +0530

    Feat: added search videos functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,44 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { Paper, IconButton } from "@mui/material";
-import { Search } from "@mui/icons-material";
+import { Paper, IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`./search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
-        border: "1px solid #e3e3e3",
+        border: '1px solid #e3e3e3',
         p1: 2,
-        boxShadow: "none",
+        boxShadow: 'none',
         mr: { sm: 5 },
-      }}>
+      }}
+    >
       <input
         className="search-bar"
         placeholder="Search.."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={e => {
+          setSearchTerm(e.target.value);
+        }}
       />
-      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+      <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
         <Search />
       </IconButton>
     </Paper>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,58 +1,30 @@
-import React from 'react';
-
 import { useState, useEffect } from 'react';
-import { Box, Stack, Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { Box, Typography } from '@mui/material';
 
 import { fetchFromAPI } from '../utils/fetchFromAPI';
-import { Sidebar, Videos } from './';
+import { Videos } from './';
 
-const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState('New');
+const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then(data =>
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then(data =>
       setVideos(data.items),
     );
-  }, [selectedCategory]);
+  }, [searchTerm]);
 
   return (
-    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
-      <Box
-        sx={{
-          height: { sx: 'auto', md: '92vh' },
-          borderRight: '1px solid #3d3d3d',
-          px: { sx: 0, md: 2 },
-        }}
-      >
-        <Sidebar
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+        Search Results for:{' '}
+        <span style={{ color: `#F31503` }}>{searchTerm}</span> videos
+      </Typography>
 
-        <Typography
-          className="copyright"
-          variant="body2"
-          sx={{ mt: 1.5, color: '#fff' }}
-        >
-          Copyright 2023 Amol Shelke
-        </Typography>
-      </Box>
-
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-        <Typography
-          variant="h4"
-          fontWeight="bold"
-          mb={2}
-          sx={{ color: 'white' }}
-        >
-          {selectedCategory} <span style={{ color: `#F31503` }}>Videos</span>
-        </Typography>
-
-        <Videos videos={videos} />
-      </Box>
-    </Stack>
+      <Videos videos={videos} />
+    </Box>
   );
 };
 
-export default Feed;
+export default SearchFeed;
