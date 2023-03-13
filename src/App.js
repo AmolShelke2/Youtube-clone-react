fde717e499daf798f652c389249c9a8f26762942
Amol shelke <shelkeamol897@gmail.com>
2023-03-13 23:28:16 +0530

    Updated Route typo mistake

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
-        <Route path="/video/:id" exact element={<VideoDetail />} />
-        <Route path="/channel/:id" exact element={<ChannelDetail />} />
-        <Route path="/search/:searctTerm" exact element={<SearchFeed />} />
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
