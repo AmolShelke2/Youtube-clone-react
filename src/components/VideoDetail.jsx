a1373eefaad6be4545c68c5a29b8e02f6afd23fb
Amol shelke <shelkeamol897@gmail.com>
2023-03-13 23:40:37 +0530

    Creating Video Play functionality with video details

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { Video } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   return <div>VideoDetail</div>;
