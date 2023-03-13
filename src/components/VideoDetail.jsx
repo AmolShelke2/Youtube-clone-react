e9d316f092d0a46e41e19cfd9b08e54f1496176e
Amol shelke <shelkeamol897@gmail.com>
2023-03-14 00:09:09 +0530

    Fetched A video and added video playing functionality

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -5,18 +5,23 @@ import { Typography, Box, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
 import { Video } from './';
+
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+
   const { id } = useParams();
 
   useEffect(() => {
-    fetchFromAPI();
+    fetchFromAPI(`videos?part=snippet.statistics&id=${id}`).then(data =>
+      setVideoDetail(data.items[0]),
+    );
   }, [id]);
 
   return (
     <Box minHeight="95vh">
-      <Stack direction={{ xs: 'column', md: row }}>
+      <Stack direction={{ xs: 'column', md: 'row' }}>
         <Box flex={1}>
           <Box
             sx={{
@@ -25,7 +30,11 @@ const VideoDetail = () => {
               top: '86px',
             }}
           >
-            <ReactPlayer />
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
           </Box>
         </Box>
       </Stack>
